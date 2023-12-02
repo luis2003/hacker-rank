e9f76fed9fbbdb1822e173373bc43d5d2c6d3522
luis2003 <luis2003@gmail.com>
2023-12-02 17:13:59 -0300

    Weird: Add test for 2nd condition

--- source/weird.py
+++ source/weird.py
@@ -1,2 +1,11 @@
-def determine_weirdness(input):
-    return "Weird"
+def determine_weirdness(input_value: int):
+    if _is_even(input_value):
+        return "Weird"
+    else:
+        return "Not Weird"
+
+
+def _is_even(value: int) -> bool:
+    if value % 2 != 0:
+        return True
+    return False
--- tests/test_weird.py
+++ tests/test_weird.py
@@ -4,7 +4,7 @@ from .context import weird
 
 
 class WeirdTestCase(BaseTestStdout):
-    def test_determine_weirdness_odd(self):
+    def test_odd_is_weird(self):
         # Arrange
         input_value = 3
         expected_output = "Weird"
@@ -13,6 +13,14 @@ class WeirdTestCase(BaseTestStdout):
         result = weird.determine_weirdness(input_value)
 
         # Assert
+        self.assertEqual(result, expected_output)
+
+    def test_even_two_to_five_not_weird(self):
+        input_value = 2
+        expected_output = "Not Weird"
+
+        result = weird.determine_weirdness(input_value)
+
         self.assertEqual(result, expected_output)
 
 
