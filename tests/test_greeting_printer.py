f6fa04a7c712da126fc570b4a9b8e117769c6314
luis2003 <luis2003@gmail.com>
2023-12-02 08:25:30 -0300

    Inital GreetingPrinter with failing tests

---
+++ tests/test_greeting_printer.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+from io import StringIO
+from main import GreetingPrinter
+
+
+class TestGreetingPrinter(unittest.TestCase):
+    def setUp(self):
+        # self.original_stdout = sys.stdout
+        self.held_output = StringIO()
+        sys.stdout = self.held_output
+
+    def tearDown(self) -> None:
+        # sys.stdout = self.original_stdout
+        sys.stdout = sys.__stdout__
+
+    def test_print_two_part_greeting(self):
+        # Arrange
+        greeting_str_part1 = "Hello"
+        greeting_str_part2 = "World"
+        expected_output = "Hello, World!"
+
+        # Act
+        a_greeting_printer = GreetingPrinter()
+        a_greeting_printer.print_two_part_greeting(greeting_str_part1, greeting_str_part2)
+
+        self.assertEqual(self.held_output.getvalue(), expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
