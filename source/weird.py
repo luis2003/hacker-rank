f32b938630400f6100642272b5a534961c6d5319
luis2003 <luis2003@gmail.com>
2023-12-02 17:47:13 -0300

    Weird: Change logic to print and adap tests

--- source/weird.py
+++ source/weird.py
@@ -1,12 +1,12 @@
 def determine_weirdness(input_value: int):
     if _is_even(input_value):
-        return "Weird"
+        print("Weird")
     elif _in_six_to_twenty_inclusive_range(input_value):
-        return "Weird"
+        print("Weird")
     elif _is_greater_than_twenty(input_value):
-        return "Not Weird"
+        print("Not Weird")
     else:
-        return "Not Weird"
+        print("Not Weird")
 
 
 def _is_even(value: int) -> bool:
--- tests/test_weird.py
+++ tests/test_weird.py
@@ -10,34 +10,34 @@ class WeirdTestCase(BaseTestStdout):
         expected_output = "Weird"
 
         # Act
-        result = weird.determine_weirdness(input_value)
+        weird.determine_weirdness(input_value)
 
         # Assert
-        self.assertEqual(result, expected_output)
+        self.assertEqual(expected_output, self.held_output.getvalue().rstrip())
 
     def test_even_two_to_five_not_weird(self):
         input_value = 2
         expected_output = "Not Weird"
 
-        result = weird.determine_weirdness(input_value)
+        weird.determine_weirdness(input_value)
 
-        self.assertEqual(result, expected_output)
+        self.assertEqual(expected_output, self.held_output.getvalue().rstrip())
 
     def test_even_six_to_twenty_is_weird(self):
         input_value = 6
         expected_output = "Weird"
 
-        result = weird.determine_weirdness(input_value)
+        weird.determine_weirdness(input_value)
 
-        self.assertEqual(result, expected_output)
+        self.assertEqual(expected_output, self.held_output.getvalue().rstrip())
 
     def test_even_greater_than_twenty_not_weird(self):
         input_value = 22
         expected_output = "Not Weird"
 
-        result = weird.determine_weirdness(input_value)
+        weird.determine_weirdness(input_value)
 
-        self.assertEqual(result, expected_output)
+        self.assertEqual(expected_output, self.held_output.getvalue().rstrip())
 
 
 if __name__ == '__main__':
