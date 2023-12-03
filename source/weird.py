4a6d70d001c88bad07a644002b51366420af6ed0
luis2003 <luis2003@gmail.com>
2023-12-03 08:04:20 -0300

    Weird: Change main function to return instead of printing

--- source/weird.py
+++ source/weird.py
@@ -1,8 +1,8 @@
-def determine_weirdness(input_value: int):
+def determine_weirdness(input_value: int) -> str:
     if _is_odd(input_value) or _is_in_six_to_twenty_range(input_value):
-        print("Weird")
+        return "Weird"
     else:
-        print("Not Weird")
+        return "Not Weird"
 
 
 def _is_odd(value: int) -> bool:
@@ -13,3 +13,5 @@ def _is_in_six_to_twenty_range(value: int) -> bool:
     return 6 <= value <= 20
 
 
+if __name__ == '__main__':
+    print(determine_weirdness(int(input("enter an integer value: "))))
