ae3755bbcbe483f29820038519cac385cbd40ce8
luis2003 <luis2003@gmail.com>
2023-12-03 08:09:19 -0300

    Weird: Fix tests for function returning instead of printing

--- tests/test_weird.py
+++ tests/test_weird.py
@@ -1,43 +1,42 @@
 import unittest
-from .base_test_stdout import BaseTestStdout
 from .context import weird
 
 
-class WeirdTestCase(BaseTestStdout):
+class WeirdTestCase(unittest.TestCase):
     def test_odd_is_weird(self):
         # Arrange
         input_value = 3
         expected_output = "Weird"
 
         # Act
-        weird.determine_weirdness(input_value)
+        actual_output = weird.determine_weirdness(input_value)
 
         # Assert
-        self.assertEqual(expected_output, self.held_output.getvalue().rstrip())
+        self.assertEqual(expected_output, actual_output)
 
     def test_even_two_to_five_not_weird(self):
         input_value = 2
         expected_output = "Not Weird"
 
-        weird.determine_weirdness(input_value)
+        actual_output = weird.determine_weirdness(input_value)
 
-        self.assertEqual(expected_output, self.held_output.getvalue().rstrip())
+        self.assertEqual(expected_output, actual_output)
 
     def test_even_six_to_twenty_is_weird(self):
         input_value = 6
         expected_output = "Weird"
 
-        weird.determine_weirdness(input_value)
+        actual_output = weird.determine_weirdness(input_value)
 
-        self.assertEqual(expected_output, self.held_output.getvalue().rstrip())
+        self.assertEqual(expected_output, actual_output)
 
     def test_even_greater_than_twenty_not_weird(self):
         input_value = 22
         expected_output = "Not Weird"
 
-        weird.determine_weirdness(input_value)
+        actual_output = weird.determine_weirdness(input_value)
 
-        self.assertEqual(expected_output, self.held_output.getvalue().rstrip())
+        self.assertEqual(expected_output, actual_output)
 
 
 if __name__ == '__main__':
