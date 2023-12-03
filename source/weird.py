a1a61a414bc729b9dd5e5ccec68dff69170e444a
luis2003 <luis2003@gmail.com>
2023-12-03 08:21:04 -0300

    Weird: Add Docstrings

--- source/weird.py
+++ source/weird.py
@@ -1,4 +1,8 @@
 def determine_weirdness(input_value: int) -> str:
+    """
+        Determine whether the given input value is weird or not.
+        Returns "Weird" if the value is odd or in the range [6, 20], otherwise "Not Weird".
+    """
     if _is_odd(input_value) or _is_in_six_to_twenty_range(input_value):
         return "Weird"
     else:
@@ -6,10 +10,16 @@ def determine_weirdness(input_value: int) -> str:
 
 
 def _is_odd(value: int) -> bool:
+    """
+    Check if the given value is odd.
+    """
     return value % 2 != 0
 
 
 def _is_in_six_to_twenty_range(value: int) -> bool:
+    """
+    Check if the given value is in the range [6, 20].
+    """
     return 6 <= value <= 20
 
 
