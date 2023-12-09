a8a4762e046b7662b34867d4de1290fc292452c3
luis2003 <luis2003@gmail.com>
2023-12-09 10:29:04 -0300

    Loop: Add basic docstring

--- source/loop.py
+++ source/loop.py
@@ -1,2 +1,7 @@
 def get_previous_squares(input_value: int) -> list:
+    """
+    calculate the squares of all non-negative integers less than the input value
+    :param input_value: an integer
+    :return: a list containing the squares of all non-negative integers less than input
+    """
     return [0, 1, 4]
